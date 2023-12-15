f5f47851e17bc6e84b7a543a4838887651d21d0b
Phil Chen <06fahchen@gmail.com>
2023-12-15 22:24:16 +0800

    feat: add supervisor

--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule BreezeRt.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {BreezeRt.Application, []}
     ]
   end
 
